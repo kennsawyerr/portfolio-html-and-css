8c3d48292785c71dffc646e126ba3e21f6809df0
Kehinde Sawyerr <114794974+kennsawyerr@users.noreply.github.com>
2023-06-19 23:23:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// first create media queries
+//
+//i dont understand how to solve this problem oo. 
+/*     I NEED TO HIDE THIS BUTTON WHEN ITS ON LARGE SCREENS OR ONLY SHOW IT ON LARGE SCREENS
+
+once ul is on small screens its display:block
+
+nav buttons in small screen
+how to control flex wrap
+how to move my navbar to b responsive esp RHS
+my project img is not filling screen size
+i need all the elements to fill the same size no matter the img size
+
+
+
+
+
+*/
+
+
+const navBtn = document.getElementById("nav-btn")
+
+
+navBtn.addEventListener("click", function (){
+    navBtn.classList.toggle("show-li")
+})
