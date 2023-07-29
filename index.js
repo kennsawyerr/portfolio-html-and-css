3e3927928033b924e63768fcda037b7b8cc1d6b4
Kehinde Sawyerr <114794974+kennsawyerr@users.noreply.github.com>
2023-07-29 22:44:49 +0100

    modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-
+let slideIndex = 0;
+SlideShow()
 
 
 const navBtn = document.getElementById("nav-btn")
 const navList =document.querySelector(".nav-list")
 
-navBtn.addEventListener("click", function (){
+navBtn.addEventListener("click", function(){
     navList.classList.toggle("active");
+    animateBtn();
+    
 })
 
 document.querySelector('header').style.height = document.querySelector('.navbar').getBoundingClientRect().height + 'px';
+
+
+
 
 
   /*    portfolio section 5
@@ -17,4 +23,22 @@ document.querySelector('header').style.height = document.querySelector('.navbar'
     port project page 8
     remove projects btn 9
     sneskers dropdown cart
+
   */
+
+    function SlideShow(){
+      let i;
+      let slides =document.getElementsByClassName("Slides");
+      for (let i= 0; i< slides.length; i++){
+
+          slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+     slides[slideIndex-1].style.display = "block";
+    setTimeout(SlideShow, 5000); // Change image in seconds
+    }
+
+  function animateBtn(){
+    document.getElementsByClassName("bar").classList.toggle("change")
+  }
